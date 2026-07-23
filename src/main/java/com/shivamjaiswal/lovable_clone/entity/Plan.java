59e217daaa5b378bf0529fcb05db135442f99719
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-07-23 10:15:34 +0530

    plan Table Created

---
+++ src/main/java/com/shivamjaiswal/lovable_clone/entity/Plan.java
@@ -0,0 +1,21 @@
+package com.shivamjaiswal.lovable_clone.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Plan {
+    Long id;
+    String name;
+
+    String stripePriceId;
+    Integer maxProjects;
+    Integer maxTokenPerDay;
+    Integer maxPreview;
+    Boolean unlimitedAi;
+    
+}
