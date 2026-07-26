3e11708d3c5319816b1de0317002b48617d71375
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-07-26 09:54:36 +0530

    projectFile Table Created

---
+++ src/main/java/com/shivamjaiswal/lovable_clone/entity/ProjectFile.java
@@ -0,0 +1,26 @@
+package com.shivamjaiswal.lovable_clone.entity;
+
+import java.time.Instant;
+
+public class ProjectFile {
+
+    Long Id;
+
+    Project project;
+
+    String path;
+
+    String minIoObjectKey;
+
+    User CreatedBy;
+
+    User UpdatedBy;
+
+    Instant createdAt;
+
+    Instant updatedAt;
+
+
+
+
+}
